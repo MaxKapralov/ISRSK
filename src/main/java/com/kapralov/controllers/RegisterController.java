c529eda05b7a7d3027b4ab6875dfc0ac69ff2f69
Max Kapralov <maxkapralov997@gmail.com>
2017-12-17 19:46:42 +0100

    Dodano formularz rejestracji i prosty kontroller obslugujacy formularz.

---
+++ src/main/java/com/kapralov/controllers/RegisterController.java
@@ -0,0 +1,40 @@
+package com.kapralov.controllers;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kapralov.model.data.NewUserForm;
+
+@Controller
+public class RegisterController {
+
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+    public String viewRegistration(Map<String, Object> model)
+    {
+        NewUserForm userForm = new NewUserForm();
+        model.put("userForm", userForm);
+        
+        return "register";
+    }
+    
+    @RequestMapping(value="/register", method=RequestMethod.POST)
+    public String processRegistration(@ModelAttribute("userForm") @Valid NewUserForm user, BindingResult result, Map<String, Object> model)
+    {
+        if(result.hasErrors())
+            return "register";
+        return "redirect:/result";
+    }
+    
+    @RequestMapping(value="/result", method=RequestMethod.GET)
+    public String result()
+    {
+    	return "result";
+    }
+}
---
+++ src/main/java/com/kapralov/model/data/NewUserForm.java
@@ -0,0 +1,89 @@
+package com.kapralov.model.data;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class NewUserForm {
+
+	@NotNull @Size(min = 1,  max = 20)
+	private String name;
+	
+	@NotNull @Size(min = 1,  max = 20)
+	private String surname;
+	
+	@NotNull @Size(min = 4,  max = 20)
+	private String login;
+	
+	//@PasswordConstraint
+	private String password;
+	
+	//@PasswordConstraint
+	private String passAgain;
+	
+	@Email @NotNull 
+	private String email;
+	
+	@DateTimeFormat(pattern = "dd.MM.YYYY")
+	private Date birthday;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassAgain() {
+		return passAgain;
+	}
+
+	public void setPassAgain(String passAgain) {
+		this.passAgain = passAgain;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+}
