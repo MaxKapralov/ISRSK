3067546c8f9f616db733af1d8912ad967db14abf
Max Kapralov <maxkapralov997@gmail.com>
2017-12-27 19:35:08 +0100

    Dodano klase NewRoomRepository

---
+++ src/main/java/com/kapralov/model/repository/NewRoomRepository.java
@@ -0,0 +1,14 @@
+package com.kapralov.model.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kapralov.model.data.NewRoom;
+
+@Repository
+public interface NewRoomRepository extends CrudRepository<NewRoom, Long>{
+
+	NewRoom save(NewRoom room);
+	Iterable<NewRoom> findAll();
+	NewRoom findByLocation(String location);
+}
